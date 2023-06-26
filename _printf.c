b048e918041b04eef06dc1bdd9fd3e9aba47fa00
semilore <semilore@omotade-michaels.com>
2023-06-26 17:07:12 +0100

    add: handles %c and %s

--- _printf.c
+++ _printf.c
@@ -10,4 +10,39 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	unsigned int i;
+	va_start(args, format);
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	i = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{	
+				int c;
+
+				c = va_arg(args, int);
+				_putchar(c);
+				i++;
+			}
+			else if (format[i] == 's')
+			{
+				char *s;
+
+				s = va_arg(args, char*);
+				if (s == NULL)
+					_puts("nil");
+				_puts(s);
+				i++;
+			}
+		}
+		_putchar(format[i]);
+		i++;
+	}
+	return (i);
 }
--- _puts.c
+++ _puts.c
@@ -13,5 +13,5 @@ void _puts(char *str)
 		_putchar(*str);
 		str++;
 	}
-	_putchar('\n');
+	/* _putchar('\n'); */
 }
