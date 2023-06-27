e93336e794a068d5a9045aa91dc938560d963b3b
semilore <semilore@omotade-michaels.com>
2023-06-27 18:33:41 +0100

    test

--- _printf.c
+++ _printf.c
@@ -10,14 +10,14 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int charCount, i, r, j;
-	/*format_t format_arr[] = {
+	format_t format_arr[] = {
 		{"c", format_c},
 		{"s", format_s},
 		{"d", format_d},
 		{"i", format_d},
 		{"%", format_percent},
 		{NULL, NULL}
-	};*/
+	};
 
 	if (format == NULL)
 	{
