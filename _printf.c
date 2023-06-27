f916256ade02efb1df5c12eca6e1090cd2b2ddd7
TheGreatestbug <izyspikes@gmail.com>
2023-06-27 17:52:09 +0100

    Error Handling

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int charCount, i, r;
+	int charCount, i, r, j;
 	/*format_t format_arr[] = {
 		{"c", format_c},
 		{"s", format_s},
@@ -26,14 +26,41 @@ int _printf(const char *format, ...)
 	charCount = 0;
 	r = 0;
 	i = 0;
+	j = 0;
 	va_start(args, format);
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
-			int (*format_fun)(va_list) = NULL;
+			/*int (*format_fun)(va_list) = NULL;*/
 
-			if (format[i + 1] == '\0')
+			while (format_arr[j].c)
+			{
+				if (format[i + 1] == format_arr[j].c[0])
+				{
+					r = format_arr[j].fun(args);
+					if (r == -1)
+						return (-1);
+					charCount += r;
+					break;
+				}
+				j++;
+			}
+			if (!format_arr[j].c && format[i + 1] != ' ')
+			{
+				if (format[i + 1])
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					charCount += 2;
+				}
+				else
+				{
+					return (-1);
+				}
+			}
+
+			/*if (format[i + 1] == '\0')
 				return (-1);
 			format_fun = get_format(&format[i + 1]);
 			if (format_fun == NULL)
@@ -47,7 +74,7 @@ int _printf(const char *format, ...)
 				if (r == -1)
 					return (-1);
 				charCount += r;
-			}
+			}*/
 			i++;
 		}
 		else
